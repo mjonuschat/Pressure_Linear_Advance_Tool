fbe137dc815c3ef712624e45f308850ea05652ee
Ellis <AndrewEllis93@gmail.com>
2022-11-16 15:14:08 -0500

    add 3d preview

--- pressure_advance_new/pressure_advance.js
+++ pressure_advance_new/pressure_advance.js
@@ -1181,6 +1181,82 @@ function displayCalculatedValues(action = 'show'){
   }
 }
 
+function render(gcode) {
+    // First parse the ;TYPE: comments to build an example config for the GCodeRenderer.
+    // Note that the example gcode does not indicate travel moves easily -> just ignore that fact for simplicity.
+
+    const TRANSPARENT_COLOR = new gcodeViewer.Color()
+
+    /*
+    const INNER_COLOR = new gcodeViewer.Color('#ff0000')
+    const OUTER_COLOR = new gcodeViewer.Color('#00ff00')
+    const SKIRT_COLOR = new gcodeViewer.Color('#ffff00')
+    const FILL_COLOR = new gcodeViewer.Color('#ffff00')
+    const BOTTOM_FILL_COLOR = new gcodeViewer.Color('#ffff00')
+    const INTERNAL_FILL = new gcodeViewer.Color('#00ffff')
+    const TOP_FILL = new gcodeViewer.Color('#ff00ff')
+    */
+    const DEFAULT_COLOR = new gcodeViewer.Color('#0000ff')
+    const ANCHOR_COLOR = new gcodeViewer.Color('#00ff00')
+    const TRAVEL_COLOR = new gcodeViewer.Color('#ff0000')
+
+    let colorConfig = []
+
+    gcode.split("\n").forEach(function(line, i) {
+        let color
+        /*
+        if (line.startsWith(";TYPE:WALL-INNER")) {
+            color = INNER_COLOR
+        } else if (line.startsWith(";TYPE:WALL-OUTER")) {
+            color = OUTER_COLOR
+        } else if (line.startsWith(";TYPE:SKIRT")) {
+            color = SKIRT_COLOR
+        } else if (line.startsWith(";TYPE:FILL")) {
+            color = FILL_COLOR
+        } else if (line.startsWith(";TYPE:BOTTOM-FILL")) {
+            color = BOTTOM_FILL_COLOR
+        } else if (line.startsWith(";TYPE:INTERNAL-FILL")) {
+            color = INTERNAL_FILL
+        } else if (line.startsWith(";TYPE:TOP-FILL")) {
+            color = TOP_FILL
+        }
+        */
+        
+        if (line.includes("draw anchor perimeter")) {
+          color = ANCHOR_COLOR
+        } else {
+          color = DEFAULT_COLOR
+        }
+
+        if (colorConfig.length === 0 || color && colorConfig[colorConfig.length-1].color !== color) {
+            colorConfig.push({toLine: i, color})
+        } else {
+            colorConfig[colorConfig.length - 1].toLine = i
+        }
+    });
+
+    const renderer = new gcodeViewer.GCodeRenderer(gcode, 640, 480, new gcodeViewer.Color(0x808080))
+
+    // This is an example using the Speed colorizer.
+    // Other options are:
+    // * SimpleColorizer (default) - sets all lines to the same color
+    // * SpeedColorizer - colorizes based on the speed / feed rate
+    // * TempColorizer - colorizes based on the temperature
+    renderer.colorizer = new gcodeViewer.LineColorizer(colorConfig)
+    renderer.travelWidth = 0 // disable rendering travel moves
+    
+    
+    const element = document.getElementById("gcode-viewer")
+    if (element.childElementCount < 1){
+      element.appendChild(renderer.element())
+    } else {
+      element.replaceChild(renderer.element(), element.children[0])
+    }
+    renderer.render().then(() => console.log("rendering finished"))
+
+    renderer.fitCamera()
+}
+
 // sanity checks for pattern / bed size
 function validateInput() {
   setHtmlVars();
@@ -1448,4 +1524,7 @@ $(window).load(() => {
 
   // Focus the first field
   //$('#padv input:first').focus();
+
+  genGcode();
+  render($('#gcodetextarea').val())
 });
